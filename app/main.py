a1f3e129ea659b475ad998040a9653d165670045
MartinXavierNievas <100978531+martinxaviernievas@users.noreply.github.com>
2024-06-08 20:34:34 -0300

    Finalizando la configuracion de main

--- app/main.py
+++ app/main.py
@@ -10,6 +10,32 @@ app.add_middleware(
     allow_origins=["*"]
     allow_credentials=True,
     allow_methods=["*"],
-    allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.post("/subir")
+async def subir_pdf(archivo: UploadFile, correo: str = Form(...)):
+    """
+    Carga un archivo PDF, extrae las primeras 30 lineas de texto y envia el contenido por correo electronico
+    parametro del archivo : Archivo PDF cargado
+    parametro correo : Direccion de correo electronico para enviar el contenido.
+    return: JSONResponse indicando el exito de la operacion.
+    """
+
+    if archivo.content_type != "application/pdf":
+        raise HTTPException(status_code=400, detail="Tipo de archivo invalido. Solo se permiten archivos PDF.")
+
+    contenido = await archivo.read()
+    try:
+        texto_extraido = extraer_primero_30_lineas(contenido)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error al extraer texto del PDF: {str(e)}")
+    
+    try:
+        enviar_email(correo, texto_extraido)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error al enviar el correo: {str(e)}")
+    
+    return JSONResponse(content={"exito": True, "mensaje": f"El correo se ha enviado exitosamente a {correo}"})
+
+    
