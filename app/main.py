8b35e6bc7e8a76f5a2e1b8dc09f20bc52ad745b9
MartinXavierNievas <100978531+martinxaviernievas@users.noreply.github.com>
2024-06-09 11:37:27 -0300

    Api Finalizada

--- app/main.py
+++ app/main.py
@@ -1,3 +1,39 @@
+#Parametros a tener en cuenta para resolver la prueba tecnica:
+'''
+Technical Test: Python/Node
+Objective:
+The objective of this technical test is to assess the candidate's proficiency in Python or Node.js and their ability to develop a RESTful API that works with automation libraries
+Requirements:
+Develop a RESTful API using Node or Python
+The API should include the following functionalities:
+Receive a PDF file.
+Extract the first 30 lines of the PDF.
+Send an email with the extracted content to the provided email address.
+Instructions:
+The candidate can choose between Node and Python.
+Include a README.md file with instructions for installing dependencies, running the project, and any other relevant details.
+The project should be uploaded to a public GitHub repository.
+Delivery time is 3 working days.
+Example Endpoint:
+Request (Node.js or Python):
+http
+POST /upload
+Content-Type: multipart/form-data
+
+{
+    "file": <pdf-file>,
+    "email": "example@example.com"
+}
+
+Expected Response:
+{
+    "success": true,
+    "message": "The email has been successfully sent to example@example.com"
+}
+
+
+'''
+
 from fastapi import FastAPI, UploadFile, Form, HTTPException
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
